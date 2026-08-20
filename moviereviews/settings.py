aedae5ae11bb16c1db425f64fe05b277aed025ea
isaace11 <tiacevedoa@eafit.edu.co>
2026-08-20 11:25:29 -0500

    navbar y footer

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'moviereviews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'moviereviews' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'moviereviews' / 'static',
+]
